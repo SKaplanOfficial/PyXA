057f4f96a300f1c42a01e4c1c60c6402fa43302e
Stephen Kaplan <stephen.kaplan@maine.edu>
2022-06-26 02:09:17 -0400

    Update XAErrors.py

--- PyXA/XAErrors.py
+++ PyXA/XAErrors.py
@@ -1,7 +1,15 @@
 class ApplicationNotFoundError(Exception):
-	def __init__(self, name):
+	def __init__(self, name: str):
 		self.name = name
 		Exception.__init__(self, name)
 	
 	def __str__(self):
 		return f'Application {self.name} not found.'
+
+class InvalidPredicateError(Exception):
+	def __init__(self, message: str):
+		self.message = message
+		Exception.__init__(self, message)
+
+	def __str__(self):
+		return f"Could not construct valid predicate format. {self.message}"

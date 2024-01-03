fae720efc2a0f543dbe9503f5cf9b898fb722793
Stephen Kaplan <stephen.kaplan@maine.edu>
2024-01-03 00:50:45 -0500

    Add PyXA.utils.AppBuilder

--- PyXA/Additions/Utils.py
+++ PyXA/Additions/Utils.py
@@ -3,7 +3,9 @@
 A collection of classes for interacting with macOS features in various ways.
 """
 
+import time
 import AppKit
+import ScriptingBridge
 import xml.etree.ElementTree as ET
 from typing import Union, Callable, Any, Literal
 from PyObjCTools import AppHelper
@@ -12,8 +14,155 @@ from datetime import datetime, timedelta
 from enum import Enum
 from time import sleep
 
+import PyXA.XABase
+import PyXA.XABaseScriptable
+from PyXA.XAErrors import ApplicationNotFoundError
+
+class AppBuilder():
+    """A class for constructing on-the-fly PyXA Application classes for scriptable applications that do not have a pre-defined class.
+
+    .. warning:: This is an experimental feature and may not work as expected.
+
+    .. versionadded:: 0.2.3
+    """
+    def __init__(self, name: str):
+        """Initializes a new AppBuilder instance. Does not create the associated application class.
+
+        :param name: The name of the application to build a class for.
+        :type name: str
+
+        .. versionadded:: 0.2.3
+        """
+        self.name = name
+
+        self.xa_elem = None
+        self.xa_scel = None
+
+        app_bundle_path = self.__xa_get_path_to_app(name)
+        app_bundle_url = AppKit.NSURL.fileURLWithPath_(app_bundle_path)
+        app_bundle = AppKit.NSBundle.bundleWithURL_(app_bundle_url)
+        app_bundle_id = app_bundle.bundleIdentifier()
+        self.sdef_path = app_bundle.pathsForResourcesOfType_inDirectory_("sdef", "").firstObject()
+
+        self._xa_wksp = AppKit.NSWorkspace.sharedWorkspace()
+        url = self._xa_wksp.URLForApplicationWithBundleIdentifier_(app_bundle_id)
+
+        config = AppKit.NSWorkspaceOpenConfiguration.alloc().init()
+        config.setActivates_(False)
+        config.setHides_(True)
+
+        app_ref = None
+        def _launch_completion_handler(app, _error):
+            nonlocal app_ref
+            self.xa_elem = AppKit.NSRunningApplication.runningApplicationsWithBundleIdentifier_(app_bundle_id).firstObject()
+
+            if self.sdef_path is not None:
+                self.xa_scel = ScriptingBridge.SBApplication.applicationWithURL_(app_bundle_url)
+            app_ref = 1
+        
+        self._xa_wksp.openApplicationAtURL_configuration_completionHandler_(url, config, _launch_completion_handler)
+        while app_ref is None:
+            time.sleep(0.01)
+
+    def __xa_get_path_to_app(self, app_identifier: str) -> str:
+        app_paths = self.__xa_load_app_paths()
+        candidate = None
+        for path in app_paths:
+            app_path_component = path.split("/")[-1][:-4]
+            if app_identifier.lower() == path.lower() or app_identifier.lower() == app_path_component.lower():
+                return path
+            
+            if app_identifier.lower() in path.lower():
+                candidate = path
+        
+        if candidate is not None:
+            return candidate
+
+        raise ApplicationNotFoundError(app_identifier)
+
+    def __xa_load_app_paths(self):
+        search = XABase.XASpotlight()
+        search.predicate = "kMDItemContentType == 'com.apple.application-bundle'"
+        search.run()
+        return [x.path for x in search.results]
+    
+    def application(self):
+        """Creates and instantiates a new PyXA Application class for the application specified in the AppBuilder's name attribute.
+
+        :return: An instance of the newly created PyXA Application class.
+        :rtype: PyXA.Application
+        """
+        parser = SDEFParser(self.sdef_path)
+        parser.parse()
+
+        app_class = XABase.XAApplication
+        classes = {}
+
+        def create_class(class_name: str, parent_classes: list[type], class_dict: dict):
+            new_class = type(class_name, parent_classes, class_dict)
+            setattr(self, class_name, new_class)
+            return new_class
+
+        for suite in parser.scripting_suites:
+            for scripting_class in suite["classes"]:
+                list_class_dict = {}
+                list_class_dict["__doc__"] = "A wrapper around lists of " + scripting_class["name"].lower() + "s that employs fast enumeration techniques. All properties of " + scripting_class["name"].lower() + "s can be called as methods on the wrapped list, returning a list containing each " + scripting_class["name"].lower() + "'s value for the property.\n\n.. versionadded:: " + XABase.VERSION
+
+                def __init__(self, *args):
+                    super(XABase.XAList, self).__init__()
+                    self.xa_elem = args[0]["element"]
+                    self.xa_scel = args[0]["appref"]
+                    self.xa_prnt = args[0]["parent"]
+                    self.xa_ocls = args[1]
+
+                list_class_dict["__init__"] = lambda self, properties, filter: __init__(self, properties, classes[scripting_class["name"]], filter)
+
+                for property in scripting_class["properties"]:
+                    list_class_dict[property["name"]] = lambda self, property = property: list(self.xa_elem.arrayByApplyingSelector_(property["name"]))
+
+                cls = create_class(scripting_class["name"].replace(" ", "") + "List", (XABase.XAList, ), list_class_dict)
+                classes[scripting_class["name"].replace(" ", "") + "List"] = cls
+
+                class_dict = {}
+                class_dict["__doc__"] = scripting_class["comment"] + "\n\n.. versionadded:: " + XABase.VERSION
+
+                for property in scripting_class["properties"]:
+                    class_dict[property["name"]] = lambda self, property = property: self.xa_elem.__getattribute__(property["name"])()
+
+                for element in scripting_class["elements"]:
+                    class_dict[element["name"]] = lambda self, filter = None, element = element: self._new_element(self.xa_elem.__getattribute__(element["name"])(), classes[element["type"].replace(" ", "") + "List"], filter)
+
+                for command in scripting_class["responds-to"]:
+                    if command in suite["commands"]:
+                        class_dict[suite["commands"][command]["name"]] = lambda self, suite = suite, **kwargs: self.xa_elem.__getattribute__(suite["commands"][command]["name"])(**kwargs)
+                    
+                cls = create_class(scripting_class["name"], (XABase.XAObject, ), class_dict)
+                classes[scripting_class["name"]] = cls
+                if scripting_class["name"].endswith("Application"):
+                    app_class = cls
+
+        properties = {
+            "parent": None,
+            "element": self.xa_scel,
+            "appref": self.xa_elem,
+        }
+        
+        return app_class(properties)
+        
+
 class SDEFParser():
+    """A class for parsing SDEF files and generating Python code for interacting with scriptable applications.
+
+    .. versionadded:: 0.1.1
+    """
     def __init__(self, sdef_file: Union['XABase.XAPath', str]):
+        """Initializes a new SDEFParser instance.
+
+        :param sdef_file: The full path to the SDEF file to parse.
+        :type sdef_file: Union[XABase.XAPath, str]
+
+        .. versionadded:: 0.1.1
+        """
         if isinstance(sdef_file, str):
             sdef_file = XABase.XAPath(sdef_file)
         self.file = sdef_file #: The full path to the SDEF file to parse
@@ -22,6 +171,13 @@ class SDEFParser():
         self.scripting_suites = []
 
     def parse(self):
+        """Parses the SDEF file specified in the SDEFParser's file attribute.
+
+        :return: A list of scripting suites, each containing a list of classes and a dictionary of commands.
+        :rtype: list[dict[str, Any]]
+
+        .. versionadded:: 0.1.1
+        """
         app_name = self.file.path.split("/")[-1][:-5].title()
         xa_prefix = "XA" + app_name
 
@@ -211,6 +367,11 @@ class SDEFParser():
         return suites
 
     def export(self, output_file: Union['XABase.XAPath', str]):
+        """Exports the scripting suites parsed from the SDEF file to a Python module.
+
+        :param output_file: The full path to the file to export module code to.
+        :type output_file: Union[XABase.XAPath, str]
+        """
         if isinstance(output_file, XABase.XAPath):
             output_file = output_file.path
 
